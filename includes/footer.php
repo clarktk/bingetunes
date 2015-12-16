48207d637287d498959a8f8e7ca8f35725e658d9
Terrance <Terrance@ADVP_TClark>
2015-12-16 11:58:11 -0400

    added file

--- includes/footer.php
+++ includes/footer.php
@@ -8,21 +8,21 @@
         <div class="row">
 
             <div class="col-lg-8" align="center">
-                <div><p></p></div>
-                <div id="TickerBC57937577" class="bc_ticker" style="width: 100%;overflow: auto;overflow-x: hidden;border-style: none;border-width: 0;border-color: #000000;background-color: #000000;">
-                    <table width="100%">
-                        <tr align="center">
-                            <td width="10" style="vertical-align: top;" style="color: #0000CC; font-size: 14pt; font-family: Arial,Helvetica;"><a id="TickerPrevBC57937577" href="javascript: ;" onclick="PrevTickerBC57937577();" class="bc_tickerarrow" style="text-decoration: none; color: #0000CC;">&laquo;</a></td>
-                            <td id="TickerContentBC57937577" class="bc_tickercontent" style="vertical-align: top;background-color: #000000;">
-                                <a id="TickerLinkBC57937577" href="javascript: ;" class="bc_tickerlink" style="text-decoration: none;" target="_top"><b><span id="TickerTitleBC57937577" class="bc_tickertitle" style="color: red; font-size: 14pt; font-family: Arial,Helvetica;"></span></b></a>			<span id="TickerSummaryBC57937577" class="bc_tickersummary" style="color: #000000; font-size: 10pt; font-family: Arial,Helvetica;"></span>
-                            </td>
-                            <td  width="10" style="vertical-align: top;" style="color: red; font-size: 14pt; font-family: Arial,Helvetica;"><a id="TickerNextBC57937577" href="javascript: ;" onclick="NextTickerBC57937577();" class="bc_tickerarrow" style="text-decoration: none; color: #0000CC;">&raquo;</a></td>
-                        </tr>
-                    </table>
-                    <!-- DO NOT CHANGE OR REMOVE THE FOLLOWING NOSCRIPT SECTION OR THE BLASTCASTA NEWS TICKER WILL NOT FUNCTION PROPERLY. -->
-                    <noscript><a href="http://www.blastcasta.com/" title="News Ticker by BlastCasta"><img src="http://www.poweringnews.com/images/tp.gif" border="0" /></a></noscript>
+                <div class="ticker">
+                    <div id="TickerBC57937577" class="bc_ticker" style="width: 80%; overflow: auto; overflow-x: hidden; ">
+                        <table width="100%">
+                            <tr align="center">
+                                <td width="10" style="vertical-align: top;" style="color: red; font-size: 14pt; font-family: Arial,Helvetica;"><a id="TickerPrevBC57937577" href="javascript: ;" onclick="PrevTickerBC57937577();" class="bc_tickerarrow" style="text-decoration: none; color: red;">&laquo;</a></td>
+                                <td id="TickerContentBC57937577" class="bc_tickercontent" style="vertical-align: top;">
+                                    <a id="TickerLinkBC57937577" href="javascript: ;" class="bc_tickerlink" style="text-decoration: none;" target="_top"><b><span id="TickerTitleBC57937577" class="bc_tickertitle" style="color: red; font-size: 14pt; font-family: Arial,Helvetica;"></span></b></a>			<span id="TickerSummaryBC57937577" class="bc_tickersummary" style="color: #000000; font-size: 10pt; font-family: Arial,Helvetica;"></span>
+                                </td>
+                                <td  width="10" style="vertical-align: top;" style="font-size: 14pt; font-family: Arial,Helvetica;"><a id="TickerNextBC57937577" href="javascript: ;" onclick="NextTickerBC57937577();" class="bc_tickerarrow" style="text-decoration: none; color: red;">&raquo;</a></td>
+                            </tr>
+                        </table>
+                        <!-- DO NOT CHANGE OR REMOVE THE FOLLOWING NOSCRIPT SECTION OR THE BLASTCASTA NEWS TICKER WILL NOT FUNCTION PROPERLY. -->
+                        <noscript><a href="http://www.blastcasta.com/" title="News Ticker by BlastCasta"><img src="http://www.poweringnews.com/images/tp.gif" border="0" /></a></noscript>
+                    </div>
                 </div>
-
                 <script id="scr57937577" type="text/javascript"></script>
                 <script type="text/javascript"> /* <![CDATA[ */
                     setTimeout('document.getElementById(\'scr57937577\').src = (document.location.protocol == \'https:\' ? \'https\' : \'http\') + \'://www.poweringnews.com/ticker-js.aspx?feedurl=http%3A//www.blastcasta.com/rss.aspx&changedelay=5&maxitems=-1&showsummary=0&objectid=57937577\'', 500);
